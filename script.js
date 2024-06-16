6755afa65dd9e47db5c0fa193366e3d91f6d87cc
Chrisolivieri <christian.olivieri93@gmail.com>
2024-06-16 13:19:23 +0200

    add cart

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+fetch("https://striveschool-api.herokuapp.com/api/product/", {
+    headers: {
+        "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NjY4NjJiOThmYzBmMzAwMTU1ZTVhZjMiLCJpYXQiOjE3MTgxMTcwNDksImV4cCI6MTcxOTMyNjY0OX0.XdkzLUGrc70DZI1lO4GINc99NUPwSMNi559Y6v9EcP8",
+        "Content-Type": "application/json"
+    },
+}).then((response) => {
+    response.json()
+    .then((games) => {
+        console.log(games)
+    })
+})
+
 window.onload = getGames()
 
 async function getGames() {
@@ -8,21 +20,24 @@ async function getGames() {
         }
     })
     const games = await response.json()
-    console.log(games)
+    
 
     let gamesContainer = ""
 
     games.map((game) => {
+        let stringaJSON = JSON.stringify(game)
+        let nuovaStringa = stringaJSON.replace(/'/g, "")
         gamesContainer += `
         <div class="card" style="width: 18rem;">
   <img src=${game.imageUrl} class="card-img-top" alt="...">
   <div class="card-body">
     <h5 class="card-title">${game.name}</h5>
     <p class="card-text">${game.price} €</p>
+    <a href="./details.html?name=${game.name}&imageUrl=${game.imageUrl}&brand=${game.brand}&description=${game.description}&price=${game.price}" class="card-link">Dai un'occhiata</a>
   </div>
   
   <div class="card-body">
-    <a href="./details.html?name=${game.name}&imageUrl=${game.imageUrl}&brand=${game.brand}&description=${game.description}&price=${game.price}" class="card-link">Dai un'occhiata</a>
+    
     <button class="btn btn-danger" onclick="deleteGame2('${game._id}')"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
                     <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
                     <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
@@ -33,6 +48,10 @@ async function getGames() {
                     <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z"/>
                   </svg>  
                 </button>
+                <button class="btn btn-warning" onclick= 'aggiungiAlCarrello(${nuovaStringa})' > <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cart-plus" viewBox="0 0 16 16">
+  <path d="M9 5.5a.5.5 0 0 0-1 0V7H6.5a.5.5 0 0 0 0 1H8v1.5a.5.5 0 0 0 1 0V8h1.5a.5.5 0 0 0 0-1H9z"/>
+  <path d="M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1zm3.915 10L3.102 4h10.796l-1.313 7zM6 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0m7 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0"/>
+</svg> </button>
   </div>
   
 </div>
@@ -222,3 +241,42 @@ function search() {
     })
 
 }
+
+let carrello = []
+let giochiNelCarrello = 0
+function aggiungiAlCarrello(game) {
+    console.log(game)
+    game.description = game.description.replace(/'/g, "\\'")
+    carrello.push(game)
+    alert("Gioco aggiunto al carrello")
+    giochiNelCarrello++
+    aggiornaCarrello()
+  
+  }
+
+  function aggiornaCarrello() {
+
+    let div = document.getElementById("cart")
+    let number = document.getElementById("numberGame")
+  div.innerHTML = ""
+  number.innerHTML = `<span class='badge bg-secondary'>Giochi nel carrello: ${giochiNelCarrello}  </span>`
+
+  carrello.forEach((game) => {
+    div.innerHTML += `<div class="card m-2" style="width: 8rem;">
+  <img src=${game.imageUrl} class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">${game.name}</h5>
+    <p class="card-text">${game.price} €</p>
+  </div>`
+  })
+   
+}
+
+function svuotaCarrello() {
+    carrello = []
+    giochiNelCarrello = 0
+    let div = document.getElementById("cart")
+    div.innerHTML = ""
+    aggiornaCarrello()
+  
+  }
